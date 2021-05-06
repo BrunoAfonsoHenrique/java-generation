d568f12bacee3b5d94436889530f64bf0312657e
Bruno Afonso Henrique <bruno_afonso_henrique@outlook.com>
2021-05-06 14:07:07 -0300

    dicionado o Exercicio 05

---
+++ AulasJavaGeneration/src/ExerciciosRepeticao/ExDoWhile05.java
@@ -0,0 +1,27 @@
+package ExerciciosRepeticao;
+
+import java.util.Scanner;
+
+public class ExDoWhile05 {
+
+	public static void main(String[] args) 
+	{
+		Scanner leia = new Scanner(System.in);
+		
+		int num, soma = 0;
+		
+		do
+		{
+			System.out.print("Digite um numero: ");
+			num = leia.nextInt();
+			
+			soma += num;
+			
+		}
+		while(num != 0);
+		
+		System.out.println("\nA Soma dos números digitados é: " + soma);
+
+	}
+
+}
