e95178fcfb0e7817d369cb8be1668b3985eac7f3
Bruno Afonso Henrique <bruno_afonso_henrique@outlook.com>
2021-05-07 10:58:54 -0300

    Adicionado os programas sobre Arrays em Java

---
+++ AulasJavaGeneration/src/Turma22/ExemploArrayMatriz.java
@@ -0,0 +1,32 @@
+package Turma22;
+
+import java.util.Scanner;
+
+public class ExemploArrayMatriz {
+
+	public static void main(String[] args) 
+	{
+		float[][] notas = new float[2][2];
+		float somanotas=0, media, mediageral=0;
+		int cont=0, linha, coluna;
+		
+		Scanner leia = new Scanner(System.in);
+		
+		for(linha=0; linha<2; linha++)
+		{
+			for(coluna=0; coluna<2; coluna++)
+			{
+				System.out.print("Entre com a nota: ");
+				notas[linha][coluna] = leia.nextFloat();
+				
+				somanotas += notas[linha][coluna];
+				
+				cont++;
+			}
+		}
+		mediageral = somanotas / cont;
+		
+		System.out.printf("\nMédia geral: %.2f", mediageral);
+	}
+
+}
